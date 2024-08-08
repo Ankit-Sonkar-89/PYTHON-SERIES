c903e66945ea325812e9c352c201545865474098
Ankit-Sonkar <sonkarankit624@gmail.com>
2024-08-09 00:56:00 +0530

    pythonbasics

---
+++ Firstprogram.py
@@ -0,0 +1,28 @@
+print(" Hello Python ")
+
+Name = " Ankit Sonkar "
+Age = 23
+Price = 147
+DOB = 11/7/2005
+
+print(" My name is : " , Name )
+print(" My age is : " , Age )
+print(" My ball pen price is : " , Price )
+print("My DOB is : " , DOB )
+
+print(type(Name))
+print(type(Age))
+print(type(Price))
+print(type(DOB))
+
+a = 4
+b = 2
+
+Sum1 = a+b 
+Sum2 = a*b
+Sum3 = a-b
+Sum4  = a/b
+
+print(" First element : " , a )
+print(" Second element : " , b )
+print(" Result of two elements : " , Sum1 , Sum2 , Sum3 , Sum4 )
